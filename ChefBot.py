22db47c48d60a277991abae30a54a86b178675ab
Belmihoub-Bouthaina <149616753+Belmihoub-Bouthaina@users.noreply.github.com>
2025-04-21 16:12:44 +0100

    Update ChefBot.py

--- ChefBot.py
+++ ChefBot.py
@@ -419,7 +419,8 @@ def main():
                     st.download_button(
                         label=t["download"],
                         data=pdf,
-                        file_name=("recette.pdf" if lang == "fr" else "وصفة.pdf"),
+                        file_name=("recette.pdf" if lang == "fr" else "وصفة.pdf")
+                        font_path = os.path.join(os.path.dirname(__file__), "Amiri-Regular.ttf")
                         mime="application/pdf",
                         use_container_width=True
                     )
